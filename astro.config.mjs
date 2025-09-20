dec6e0e269d3be36181c480b86f25aaaf7e997ef
Masayuki <nitamagoex@yahoo.co.jp>
2025-09-20 22:04:25 +0900

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nitamago.github.io',
-  base: '/',
+  base: '/lol-matchup-dictionary/',
   vite: {
     plugins: [tailwindcss()],
   },
