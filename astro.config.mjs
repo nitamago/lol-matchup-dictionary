751a117accf0a1ede35ba17422092b9a3dc7dea9
Masayuki <nitamagoex@yahoo.co.jp>
2025-10-12 10:17:03 +0900

    fix config for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nitamago.github.io',
-  base: '/lol-matchup-dictionary/',
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  integrations: [sitemap()],
+  // site: 'https://nitamago.github.io',
+  // base: '/lol-matchup-dictionary/',
+
+  // vite: {
+  //   plugins: [tailwindcss()],
+  // },
+
+  // integrations: [sitemap()],
+  // adapter: netlify(),
+  output: 'server',
+  adapter: netlify(),
 });
