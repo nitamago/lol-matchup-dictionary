5f66f9717260de036159b6b142ee8e1ff862d7b2
Masayuki <nitamagoex@yahoo.co.jp>
2025-10-14 16:19:43 +0900

    add redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,16 @@ export default defineConfig({
   // adapter: netlify(),
   // output: 'static',
   redirects: {
-    '/lol-matchup-dictionary': '/'
+    '/lol-matchup-dictionary': '/',
+    '/lol-matchup-dictionary/first/top/': '/first/top/',
+    '/lol-matchup-dictionary/first/mid/': '/first/mid/',
+    '/lol-matchup-dictionary/first/jg/': '/first/jg/',
+    '/lol-matchup-dictionary/first/bot/': '/first/bot/',
+    '/lol-matchup-dictionary/first/sup/': '/first/sup/',
+    '/lol-matchup-dictionary/counter/top/': '/counter/top/',
+    '/lol-matchup-dictionary/counter/mid/': '/counter/mid/',
+    '/lol-matchup-dictionary/counter/jg/': '/counter/jg/',
+    '/lol-matchup-dictionary/counter/bot/': '/counter/bot/',
+    '/lol-matchup-dictionary/counter/sup/': '/counter/sup/',
   }
 });
